e026e235c922ec8432954fa364b63300cd20c72e
Delfos <rafael.costa@delfosim.com>
2020-08-22 18:16:26 -0300

    fixing deplot

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import AppRouter from 'base/Router'
 import './App.css';
 import InstallPWA from './PWA';
-let deferredPrompt;
 
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
   // Stash the event so it can be triggered later.
-  deferredPrompt = e;
   // Update UI notify the user they can install the PWA
   console.log("qqq");
   // showInstallPromotion();
